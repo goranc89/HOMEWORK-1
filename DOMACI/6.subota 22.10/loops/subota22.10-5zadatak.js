6ad5766a49dc34985cfb12d0413f1dcabc74fa4e
goranc89 <114668056+goranc89@users.noreply.github.com>
2022-10-30 20:32:27 +0100

    Update subota22.10-5zadatak.js

--- DOMACI/6.subota 22.10/loops/subota22.10-5zadatak.js
+++ DOMACI/6.subota 22.10/loops/subota22.10-5zadatak.js
@@ -7,3 +7,9 @@ var x = ['1', 'A', 'B', "c", "r", true, NaN, undefined];
 for (i=0;i<x.length; i++){
     console.log(x[i])
     }
+
+
+// Komentar: ovde treba da prodjes kroz ceo niz i da ga ispises u jednom redu kao string
+// Dakle, imas dodatnu promenjivu u kojoj cuvas trenutni string
+// nakon svake iteracije kroz petlju ti ces toj promenjivoj dodavati po jos jedan element niza
+// tako dobijas krajnji string :) 
